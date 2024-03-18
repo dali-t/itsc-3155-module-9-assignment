bf9c75722177270f264a9f588b2af1ef8b3f81b6
Sang Vang <vangsang2019@gmail.com>
2024-03-18 14:50:52 -0400

    Adding search_movies functionality before testing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, url_for
 
 from src.repositories.movie_repository import get_movie_repository
 
@@ -7,6 +7,41 @@ app = Flask(__name__)
 # Get the movie repository singleton to use throughout the application
 movie_repository = get_movie_repository()
 
+def populate_movie_repo():
+    # Sample movie data
+    movies_data = [
+        {
+            "title": "The Shawshank Redemption",
+            "director": "Frank Darabont",
+            "rating": 4.9
+        },
+        {
+            "title": "The Godfather",
+            "director": "Francis Ford Coppola",
+            "rating": 4.6
+        },
+        {
+            "title": "The Dark Knight",
+            "director": "Christopher Nolan",
+            "rating": 4.8
+        },
+        {
+            'title': 'Attack on Titan',
+            'director': 'Crunchyroll',
+            'rating': 4.5
+        }
+    ]
+
+    # Populate the movie repository
+    for movie_data in movies_data:
+        movie_repository.create_movie(
+            title=movie_data['title'],
+            director=movie_data['director'],
+            rating=movie_data['rating']
+        )
+
+# Call the function to populate _movie_repo
+populate_movie_repo()
 
 @app.get('/')
 def index():
@@ -31,10 +66,26 @@ def create_movie():
     return redirect('/movies')
 
 
-@app.get('/movies/search')
+@app.route('/movies/search', methods=['GET', 'POST'])
 def search_movies():
     # TODO: Feature 3
-    return render_template('search_movies.html', search_active=True)
+    # first get the movie by title
+    # Somehow get the movie's rating
+    # return that to the search_movies.html
+    if request.method == 'POST':
+        movie_search = request.form.get('movie-search')
+        movie_search_trim = movie_search.strip().lower()
+
+        movie = movie_repository.get_movie_by_title(movie_search_trim)
+
+        if movie is not None:
+            rating = movie.rating
+            movie_title = movie.title
+        else:
+            rating = -1;
+            movie_title = ''
+        return render_template('search_movies.html', search_active=True, rating=rating, movie_title=movie_title)
+    return render_template('search_movies.html', search_active=True, rating=None)
 
 
 @app.get('/movies/<int:movie_id>')
