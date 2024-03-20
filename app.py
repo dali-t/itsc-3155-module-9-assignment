1533cd8540d978d0bcb388e50b426be24f9f2480
Adalia <adalia.thao16@gmail.com>
2024-03-20 15:56:26 -0400

    First commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 
 from src.repositories.movie_repository import get_movie_repository
 
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 # Get the movie repository singleton to use throughout the application
 movie_repository = get_movie_repository()
-
 
 @app.get('/')
 def index():
@@ -16,7 +15,8 @@ def index():
 @app.get('/movies')
 def list_all_movies():
     # TODO: Feature 1
-    return render_template('list_all_movies.html', list_movies_active=True)
+    list_movies_active = movie_repository.get_all_movies()
+    return render_template('list_all_movies.html', list_movies_active=list_movies_active)
 
 
 @app.get('/movies/new')
@@ -28,7 +28,13 @@ def create_movies_form():
 def create_movie():
     # TODO: Feature 2
     # After creating the movie in the database, we redirect to the list all movies page
+    title = request.form.get("moviename")
+    director = request.form.get("directorname")
+    rating = int(request.form.get("movierating"))
+    movie_repository.create_movie(title, director, rating)
+
     return redirect('/movies')
+
 
 
 @app.get('/movies/search')
